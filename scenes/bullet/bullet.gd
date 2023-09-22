be368fb99df7764f21185816a6026f1f638897a4
Micael <mikabrytu@gmail.com>
2023-09-22 20:29:17 -0300

    Add bullet

---
+++ scenes/bullet/bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+var _parent_layer = -1
+
+
+# Public API
+
+
+func setup(spawn, direction, layer):
+	global_position = spawn.global_position
+	$"Movement System".direction = direction
+	$Sprite2D.rotation_degrees = 90 * direction.x
+	_parent_layer = layer
+
+
+# Listeners
+
+
+func _on_body_entered(body):
+	var layer = body.collision_layer
+	if layer == _parent_layer:
+		return
+	
+	queue_free()
