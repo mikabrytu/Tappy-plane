2033067d513ccd81088cf6a7f32d05aa0a1c281c
Micael <mikabrytu@gmail.com>
2023-09-24 21:12:23 -0300

    Wait some time before adding dificulty enemies on the pool

--- scenes/enemies/enemy_controller.gd
+++ scenes/enemies/enemy_controller.gd
@@ -5,13 +5,22 @@ extends Node2D
 @export var spawn_points: Array[Marker2D]
 @export var player: Node2D
 
+var _enemies_copy: Array[PackedScene]
+
+
+# Godot Messages
+
+
+func _ready():
+	_enemies_copy.append(enemies[0])
+
 
 # Implementation
 
 
 func _new_enemy():
-	var e = enemies.pick_random().instantiate()
-	var spawn = spawn_points[2]
+	var e = _enemies_copy.pick_random().instantiate()
+	var spawn = spawn_points.pick_random()
 	
 	e.set_target(player)
 	e.set_origin(spawn.get_global_position())
@@ -25,7 +34,18 @@ func _new_enemy():
 func _on_start_spawn_timer_timeout():
 	_new_enemy()
 	$"Spawn Timer".start()
+	$"Dificulty Timer".start()
 
 
 func _on_spawn_timer_timeout():
 	_new_enemy()
+
+
+func _on_dificulty_timer_timeout():
+	if _enemies_copy.size() == enemies.size():
+		return
+	
+	var index = _enemies_copy.size()
+	_enemies_copy.append(enemies[index])
+	
+	$"Dificulty Timer".start()
