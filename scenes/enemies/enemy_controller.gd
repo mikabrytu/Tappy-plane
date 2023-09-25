71dd6791ef5de44db14ff5be4dfdf199c11c452a
Micael <mikabrytu@gmail.com>
2023-09-24 21:56:47 -0300

    Add enemy type 3 to pool

--- scenes/enemies/enemy_controller.gd
+++ scenes/enemies/enemy_controller.gd
@@ -19,7 +19,8 @@ func _ready():
 
 
 func _new_enemy():
-	var e = _enemies_copy.pick_random().instantiate()
+#	var e = _enemies_copy.pick_random().instantiate()
+	var e = enemies[2].instantiate()
 	var spawn = spawn_points.pick_random()
 	
 	e.set_target(player)
@@ -34,7 +35,7 @@ func _new_enemy():
 func _on_start_spawn_timer_timeout():
 	_new_enemy()
 	$"Spawn Timer".start()
-	$"Dificulty Timer".start()
+#	$"Dificulty Timer".start()
 
 
 func _on_spawn_timer_timeout():
