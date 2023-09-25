79233c4fc6ae0319c84704f0308c5d9db354f520
Micael <mikabrytu@gmail.com>
2023-09-24 21:11:33 -0300

    Start shooting after some time on the screen

--- scenes/enemies/enemy_2.gd
+++ scenes/enemies/enemy_2.gd
@@ -4,6 +4,8 @@ extends Enemy
 
 @export var ping_pong_coords: Vector2
 
+var _is_shooting: bool = false
+
 const UP = Vector2(-0.25, -1)
 const DOWN = Vector2(-0.25, 1)
 
@@ -20,6 +22,14 @@ func _process(delta):
 		$"Movement System".direction = DOWN
 	if position.y > ping_pong_coords.y:
 		$"Movement System".direction = UP
+	
+	if target != null:
+		if (not _is_shooting 
+			and (global_position.x - target.global_position.x) < distance
+		):
+			_is_shooting = true
+			shoot()
+			$"Shoot Timer".start()
 
 
 # Listeners
