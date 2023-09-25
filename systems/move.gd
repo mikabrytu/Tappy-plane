93daf12037e9fcac3dc9d9a681d1eb3a6fa47595
Micael <mikabrytu@gmail.com>
2023-09-24 21:56:04 -0300

    Add missing move system changes

--- systems/move.gd
+++ systems/move.gd
@@ -6,17 +6,37 @@ extends Node
 @export var direction: Vector2
 @export var move: bool
 
+var _final_direction: Vector2
+var _target: Node2D
+var _follow_target: bool
+
 
 # Godot Messages
 
 
+func _ready():
+	_final_direction = direction
+
+
 func _process(delta):
 	if move:
-		get_parent().position += direction * speed * delta
+		if _target != null and _follow_target and _target.position != Vector2.ZERO:
+			_final_direction = get_parent().position.direction_to(_target.position)
+		
+		get_parent().position += _final_direction * speed * delta
 
 
 # Public API
 
 
+func move_towards(target: Node2D):
+	set_follow_target(true)
+	_target = target
+
+
 func can_move(m: bool):
 	move = m
+
+
+func set_follow_target(follow):
+	_follow_target = follow
